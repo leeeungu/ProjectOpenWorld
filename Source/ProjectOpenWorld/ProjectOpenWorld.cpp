8dde6c4076c0d6668e0f9388a147e152d115ac97
leeeungu <74060680+leeeungu@users.noreply.github.com>
2025-10-01 12:43:39 +0900

    Initial commit

---
+++ Source/ProjectOpenWorld/ProjectOpenWorld.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectOpenWorld.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectOpenWorld, "ProjectOpenWorld" );
+ 
---
+++ Source/ProjectOpenWorld/ProjectOpenWorldGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectOpenWorldGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectOpenWorldGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectOpenWorldGameMode();
+};
+
+
+
